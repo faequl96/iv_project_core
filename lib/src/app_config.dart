d4d4e32bdfa61982d589c454ed6f8f3de5371710
faiqul <faiq@voltras.co.id>
2026-05-07 15:44:39 +0700

    up gorouter

--- lib/src/app_config.dart
+++ lib/src/app_config.dart
@@ -3,7 +3,7 @@ import 'package:iv_project_core/src/enums/enums.dart';
 class AppConfig {
   AppConfig._();
 
-  static AppUserViewType _appUserViewType = AppUserViewType.general;
+  static AppUserViewType _appUserViewType = .general;
   static AppUserViewType get appUserViewType => _appUserViewType;
 
   static void set({required AppUserViewType appUserViewType}) {
